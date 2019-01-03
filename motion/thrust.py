a48f353c1dd7edf4afeb6585d8b146fcca64b5be
Mohd Omama <mohd.omama@gmail.com>
2019-01-03 10:57:34 +0530

    chore: code cleanup, variable thrust

--- motion/thrust.py
+++ motion/thrust.py
@@ -9,72 +9,81 @@ pin_b = 12
 pin_r = 23
 pin_l = 15
 
-width_h = 1600
-width_l = 1400
+thruster_pins = [pin_f, pin_b, pin_r, pin_l]
+
+width_ = 1600
+width_ = 1400
 thrust = 0
 
 pi = pigpio.pi()
 print("Initialisation. Setting pulse width to 1500")
-pi.set_servo_pulsewidth(pin_l,1500)
-pi.set_servo_pulsewidth(pin_r,1500)
-pi.set_servo_pulsewidth(pin_f,1500)
-pi.set_servo_pulsewidth(pin_b,1500)
+for pin in thruster_pins:
+    pi.set_servo_pulsewidth(pin,1500)
+
 button_delay = 0.01
 
 time.sleep(1)
 
-def motion(key, thrust):
+def hold(pins):
+    for pin in pins:
+        pi.set_servo_pulsewidth(pin,1500)
+
+
+def motion(key):
+    global thrust, thruster_pins
+
     if key == 'w':
         print("Move forward")
-        pi.set_servo_pulsewidth(pin_l,width_h)
-        pi.set_servo_pulsewidth(pin_r,width_h)
+        pi.set_servo_pulsewidth(pin_l,width_h + thrust)
+        pi.set_servo_pulsewidth(pin_r,width_h + thrust)
 
     elif key == 's':    
         print("Move backward") 
-        pi.set_servo_pulsewidth(pin_l,width_l)
-        pi.set_servo_pulsewidth(pin_r,width_l)
+        pi.set_servo_pulsewidth(pin_l,width_l - thrust)
+        pi.set_servo_pulsewidth(pin_r,width_l - thrust)
 
     elif key == 'a':    
         print("Turn left")
         pi.set_servo_pulsewidth(pin_l,1500)
-        pi.set_servo_pulsewidth(pin_r,width_h)
+        pi.set_servo_pulsewidth(pin_r,width_h + thrust)
 
     elif key == 'd':    
         print("Turn right")
-        pi.set_servo_pulsewidth(pin_l,width_h)
+        pi.set_servo_pulsewidth(pin_l,width_h + thrust)
         pi.set_servo_pulsewidth(pin_r,1500)
 
     elif key == 'i':    
         print("Move underwater")
-        pi.set_servo_pulsewidth(pin_f,width_h)
-        pi.set_servo_pulsewidth(pin_b,width_h)
+        pi.set_servo_pulsewidth(pin_f,width_h + thrust)
+        pi.set_servo_pulsewidth(pin_b,width_h + thrust)
 
     elif key == 'o':    
         print("Move upwards")
-        pi.set_servo_pulsewidth(pin_f,width_l)
-        pi.set_servo_pulsewidth(pin_b,width_l)
+        pi.set_servo_pulsewidth(pin_f,width_l - thrust)
+        pi.set_servo_pulsewidth(pin_b,width_l - thrust)
 
     elif key == 'q':
         print("Quit")
-        pi.set_servo_pulsewidth(pin_l,1500)
-        pi.set_servo_pulsewidth(pin_r,1500)
-        pi.set_servo_pulsewidth(pin_f,1500)
-        pi.set_servo_pulsewidth(pin_b,1500)
+        hold(thruster_pins)
         exit()
 
     elif key == 'h':
         print('Hold')
-        pi.set_servo_pulsewidth(pin_l,1500)
-        pi.set_servo_pulsewidth(pin_r,1500)
-        pi.set_servo_pulsewidth(pin_f,1500)
-        pi.set_servo_pulsewidth(pin_b,1500)
+        hold(thruster_pins)
+
+    elif key == '+':
+        if 0 <= thrust < 100:
+            thrust += 10
+        print('Incresed Thrust To:', thrust)
+
+    elif key == '-':
+        if 0 < thrust <= 100:
+            thrust -= 10
+        print('Decreased Thrust To:', thrust)
 
     else:
         print('None')
-        pi.set_servo_pulsewidth(pin_l,1500)
-        pi.set_servo_pulsewidth(pin_r,1500)
-        pi.set_servo_pulsewidth(pin_f,1500)
-        pi.set_servo_pulsewidth(pin_b,1500)
+        hold(thruster_pins)
 
 
 def main():
@@ -85,7 +94,6 @@ def main():
         key = it.get_user_input()
         time.sleep(0.01)
         if key != None:
-            print('The user input was', key)
             motion(key)
             it = InputThread()
             it.start()
