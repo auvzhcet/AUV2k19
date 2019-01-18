b13ed5590fe9c0bfbe5b4d551482481c2c688752
RJ722 <rahul722j@gmail.com>
2019-01-18 15:05:28 +0530

    Only move when large object detected

--- motion/line_follower.py
+++ motion/line_follower.py
@@ -5,48 +5,45 @@ from motion import movement
 
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out_image = cv2.VideoWriter('recording.avi', fourcc, 20.0, (640,480))
+out_image = cv2.VideoWriter('recording.avi', fourcc, 20.0, (640, 480))
 
 m = movement.Movement()
+
 
 def mask_image(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     lower = numpy.array([115, 0, 0])
-    upper = numpy.array([150, 255, 255])
+    upper = numpy.array([135, 255, 255])
     mask = cv2.inRange(hsv, lower, upper)
     cv2.flip(mask, 1)
     _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return mask
 
-def max_contour_center(image, mask):
-    im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    c = max(contours, key = cv2.contourArea)
-    
-    # Add contour area check
+def centroid_if_object_present(image, mask):
+    im2, contours, hierarchy = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    x,y,w,h = cv2.boundingRect(c)
+    contour = max(contours, key=cv2.contourArea)
+    contour_area = cv2.contourArea(contour)
+    print("Contour Area = ", contour_area)
 
-    cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    if contour_area > 20000:
+        print("OBJECT PRESENT")
+        x, y, w, h = cv2.boundingRect(contour)
 
-    cx, cy = (x + w/2), (y + h/2)
+        # TODO: Remove this line.
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    return cx, cy, image
+        cx, cy = (x + w/2), (y + h/2)
+        draw_centroid(image, cx, cy)
+        return cx, cy
+
+    return False
 
 
 def draw_centroid(overlay, cx, cy):
-    cv2.circle(overlay, (cx, cy), 20, (0, 0, 255), -1)
-
-
-def centroid_from_mask(image, mask):
-    M = cv2.moments(mask)
-    if M['m00'] > 0:
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-    else:
-        cx, cy = 0, 0
-    draw_centroid(image, cx, cy)
-    return cx, cy
+    cv2.circle(overlay, (int(cx), int(cy)), 20, (0, 0, 255), -1)
 
 
 def correct_error(cx, cy, image):
@@ -60,7 +57,7 @@ def correct_error(cx, cy, image):
     linear_thrust = 1600
     slope = 5/16
     rot_thrust = err*slope
-    
+
     rot_thrust = max(-100, min(rot_thrust, 100))
     if rot_thrust >= 0:
         print('Left')
@@ -75,26 +72,10 @@ def correct_error(cx, cy, image):
     m.custom_thrusts(thrusts)
 
 
-def run():
-    # NOTE: To avoid randomly occuring ZeroDivisionError and variable not
-    #       initialized error, wrap the whole thing in try/escape.
-    rec, image = cap.read()
-    # image = cv2.imread("plank_video_new.jpeg")
-    mask = mask_image(image)
-    # cx, cy = centroid_from_mask(image, mask)
-    
-    correct_error(cx, cy, image)
-
-    # Record the video for debugging purposes
-    out_image.write(image)
-
-    # cv2.imshow("window", image)
-    # key = cv2.waitKey(20)
-    # if key & 0xFF == ord('q'):
-    #     break
-
-
 def tearDown():
+    """
+    Call as API to release camera.
+    """
     global cap, out_image
     cap.release()
     out_image.release()
@@ -107,13 +88,23 @@ def main():
         # image = cv2.imread("plank_video_new.jpeg")
 
         mask = mask_image(image)
-        cx, cy = centroid_from_mask(image, mask)
-        correct_error(cx, cy, image)
+        centroid = centroid_if_object_present(image, mask)
+        if centroid:
+            (cx, cy) = centroid
+            # correct_error(cx, cy, image)
+        else:
+            print("Go left!")
+            m.left(100)
+            if centroid_if_object_present(image, mask):
+                break
 
         cv2.imshow("window", image)
+        cv2.imshow("mask", mask)
         key = cv2.waitKey(20)
         if key & 0xFF == ord('q'):
+            tearDown()
             break
+
 
 if __name__ == '__main__':
     main()
