31d2440cbe67a0205bde8901a4aacf3bb1fd7dac
RJ722 <rahul722j@gmail.com>
2019-01-23 09:01:01 +0530

    Walk straight for 5 seconds

--- test_line_follower.py
+++ test_line_follower.py
@@ -1,12 +1,27 @@
 from motion import line_follower as lf
 from motion import movement
+import time
 import os
+
 m = movement.Movement()
 
 def main():
+    tick = time.time()
+    while True:
+        try:
+            if time.time() - tick > 5:
+                m.hold()
+                time.sleep(2)
+                break
+            else:
+                m.hp_control(40)
+                m.forward(100)
+        except Exception as e:
+            print(e)
+            m.hold()
+            break
     while True:
         print('\n<--------------------->')
-        m.hp_control(40)
         try:
             lf.run()
         except KeyboardInterrupt:
